b765f2dcdc67f96c5a8c6ef57e34f4fa5a6b9b45
sangjin <loky13@daum.net>
2021-03-20 18:31:15 +0900

    search 완성

--- spring04mybatis/src/test/java/com/kh/spring04/Test04.java
+++ spring04mybatis/src/test/java/com/kh/spring04/Test04.java
@@ -1,5 +1,36 @@
 package com.kh.spring04;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.kh.spring04.entity.Student;
+
+//독립 테스트로 이름 검색을 수행
 public class Test04 {
 
+	@Test
+	public void test() throws IOException {
+		//설정파일 로딩
+		InputStream in = Resources.getResourceAsStream("mybatis/please.xml");
+		//mybatis 준비
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(in);
+		//명령 실행도구 준비(true : 자동커밋, false : 수동커밋)
+		SqlSession sqlSession = factory.openSession(true);
+				
+		//이름을 전달하여 검색 수행
+		String name = "hello";
+		
+		List<Student> list = sqlSession.selectList("student.search", name);
+		System.out.println("결과" + list.size());
+		for(Student student : list) {
+			System.out.println(student);
+		}
+	}
 }
