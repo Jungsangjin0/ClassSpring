eb9f30c2a702e6aa75a515fca9e9e3c9e811aa0f
sangjin <loky13@daum.net>
2021-04-02 17:55:11 +0900

    html 템플릿 보내기

--- spring15email/src/test/java/com/kh/spring15/Test04.java
+++ spring15email/src/test/java/com/kh/spring15/Test04.java
@@ -1,16 +1,21 @@
 package com.kh.spring15;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,14 +24,14 @@ import lombok.extern.slf4j.Slf4j;
 		"file:src/main/webapp/WEB-INF/spring/root-context.xml"
 })
 @Slf4j
-public class Test03 {
+public class Test04 {
 
 	@Autowired
 	private JavaMailSender sender;
 	
-	//목표 : 복잡한 메세지(MimeMessage)를 만들어 전송 ::첨부파일 html
+	//목표 : 미리 만들어둔 템플릿(template)을 불러와서 전송
 	@Test
-	public void test() throws MessagingException {
+	public void test() throws MessagingException, IOException {
 //		SimpleMailMessage message = new SimpleMailMessage(); 글자만 보낼 수 있음
 		
 		//메세지 생성
@@ -42,7 +47,24 @@ public class Test03 {
 		String[] to = {"loky13@daum.net", "zztkdwls@naver.com"};
 		helper.setTo(to);
 		//참조(cc), 숨은참조(bcc)설정
-		helper.setText("<h1>뻥이요요<h1>", true);//html모드 설정
+		
+		//도구를 만들어 템플릿 파일을 불러온다.
+		//classpathresource를 불러오겠다.
+		ClassPathResource resource = new ClassPathResource("email/template.html");
+		//리스트를만들었으니 파일의 스트림을 만들어 불러오기
+		File file = resource.getFile(); //java File
+		BufferedReader reader = new BufferedReader(new FileReader(file));//문자를 읽어올 때
+		//bufferedReader는 한줄 씩 읽음
+		StringBuffer buffer = new StringBuffer(); //문자열 합성기
+		
+		//reader에서 읽은 한 줄을 buffer에 계속 더한다
+		while(true) {
+			String line = reader.readLine();//1중 읽고
+			if(line == null) break; //읽은게 없으면 탈출!
+			buffer.append(line); //buffer에 line을 더해라
+		}
+		reader.close();
+		helper.setText(buffer.toString(), true);//html모드 설정
 		
 		
 		//전송
